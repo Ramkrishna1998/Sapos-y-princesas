a33ff2be11fc475dbe7407931dbe836105b2670a
ramkrishnabhatt <bhattramkrishna98@gmail.com>
2023-05-21 21:18:46 +0530

    resolve build fail issue

--- src/routes/type.ts
+++ src/routes/type.ts
@@ -1,12 +1,12 @@
 import { IndexRouteObject, NonIndexRouteObject } from 'react-router-dom';
-import { IRole } from '~/interfaces';
+//import { IRole } from '~/interfaces';
 
 export interface CustomRouteObj
   extends Omit<IndexRouteObject, 'index' | 'children'>,
     Omit<NonIndexRouteObject, 'index' | 'children'> {
   auth?: {
     requireAuth: boolean;
-    role?: IRole;
+  //  role?: IRole;
   };
   children?: CustomRouteObj[];
   idx: number;
