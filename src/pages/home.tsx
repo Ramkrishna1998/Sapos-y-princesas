0c6597cd2f2addb157b0e0dba07087f16bb0bba2
Jasmin Purani <puranijasmin994@gmail.com>
2023-06-08 14:05:46 +0530

    project Setup- included sass and classname

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,10 @@
+import CTACard from "../components/cta-card";
+
 const Home = () => {
   return (
     <div className='flex h-screen items-center justify-center bg-blue-50 text-9xl text-sky-500'>
       Home
+      <CTACard />
     </div>
   );
 };
