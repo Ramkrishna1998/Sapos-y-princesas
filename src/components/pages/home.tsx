e8420a759d139ccd90ee685112184d3c846b9d36
ramkrishnabhatt <bhattramkrishna98@gmail.com>
2023-06-09 00:25:14 +0530

    Update folder structure

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,10 +1,10 @@
-import CTACard from "../components/cta-card";
+import { ExampleComponent } from '../atoms/ExampleComponent';
 
 const Home = () => {
   return (
     <div className='flex h-screen items-center justify-center bg-blue-50 text-9xl text-sky-500'>
       Home
-      <CTACard />
+      <ExampleComponent />
     </div>
   );
 };
